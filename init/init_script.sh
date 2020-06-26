ae3731a22129999de23061fdcb69914222b89953
officialyashx <65156864+officialyashx@users.noreply.github.com>
2020-06-26 19:13:04 +0530

    Update init_script.sh

--- init/init_script.sh
+++ init/init_script.sh
@@ -26,7 +26,7 @@ echo "$1=y" >> $prog_f
 
 # Welcome Message
 welcome() {
-    echo "*****Welcome to Starry's userbot setup*****
+    echo "*****Welcome to Dedasfuck's userbot setup*****
 This Guided Setup shall help you get your own userbot up and running.
 You might be asked for sudo password several number of times."
 }
@@ -48,7 +48,7 @@ botclone() {
     cd ~
     echo "Cloning bot sources..."
     if [ -z "$bot_clone" ]; then
-        git clone https://github.com/starry69/userbot.git -b master
+        git clone https://github.com/officialyashx/userbot.git -b master
         save_prog "bot_clone"
     fi
     echo "DONE!!"
